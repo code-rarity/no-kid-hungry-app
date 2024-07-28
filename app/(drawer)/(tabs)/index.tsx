873102ccb8a6cf860b25bd228d44f95f1f09c806
Ajay Alkondon <aalkondon@AAlkondon-MacPro-16.local>
2024-07-27 23:54:30 -0500

    Working on the Story Screen

--- app/(drawer)/(tabs)/index.tsx
+++ app/(drawer)/(tabs)/index.tsx
@@ -5,6 +5,7 @@ import { ThemedView } from '@/components/ThemedView';
 
 export default function StoriesScreen() {
   const [posts, setPosts] = useState([]);
+  const [images, setImages] = useState([]);
   const [offset, setOffset] = useState(0);
 
   useEffect(() => {
@@ -12,20 +13,27 @@ export default function StoriesScreen() {
   }, []);
 
   const fetchPosts = async () => {
+    const mediaArray = [];
     try {
       // Need to set this to prod site on live and enabled JSON:API module in Drupal
       await fetch(`https://dev-no-kid-hungry.pantheonsite.io/jsonapi/node/blog?page[limit]=10&page[offset]=${offset}&include=field_image_featured.field_image&fields[file--file]=uri,url&sort=-created`)
       .then(response => response.json())
       .then(result => {
+        (result.included).map((media, j) => {
+          if(media.type == "file--file") {
+            mediaArray.push(media.attributes.uri.url);
+          }
+        });
+
         (result.data).map((post, i) => {
           setPosts(prevPosts => [
             ...prevPosts,
             {
-              id: post.id,
               title: post.attributes.title,
               date: post.attributes.field_date_simple,
               excerpt: post.attributes.field_teaser.value,
               content: post.attributes.body.processed,
+              image: 'https://dev-no-kid-hungry.pantheonsite.io' + mediaArray[i],
             }
           ]);
         });
@@ -40,27 +48,81 @@ export default function StoriesScreen() {
   }
 
   return (
-    <ThemedView style={{flex:1}}>
+    <ThemedView style={styles.container}>
       <FlatList
         data={posts}
         ListHeaderComponent={() => (
           <ThemedView>
-            <ThemedText>No Kid Hungry</ThemedText>
-            <ThemedText>Millions of kids live in hunger today. Let's end child hunger together.</ThemedText>
+            <ThemedText style={styles.h1}>No Kid Hungry</ThemedText>
+            <ThemedText style={styles.h2}>Millions of kids live in hunger today. Let's end child hunger together.</ThemedText>
           </ThemedView>
         )}
         ListFooterComponent={() => (
           <ThemedText>Load More</ThemedText>
         )}
-        keyExtractor={item => item.id.toString()}
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
         renderItem={({item, index}) => (
-          <TouchableOpacity key={item.id} onPress={() => ''} style={{flex:1}}>
-            <ThemedText>{item.date}</ThemedText>
-            <ThemedText>{item.title}</ThemedText>
-            <ThemedText>{item.excerpt}</ThemedText>
+          <TouchableOpacity onPress={() => ''} style={styles.card}>
+            <Image source={{ uri: item.image }} style={styles.img} />
+            <ThemedView style={styles.carddetails}>
+              <ThemedText style={styles.small}>{item.date}</ThemedText>
+              <ThemedText style={styles.h3}>{item.title}</ThemedText>
+              <ThemedText style={styles.p}>{item.excerpt}</ThemedText>
+            </ThemedView>
           </TouchableOpacity>
         )}
       />
     </ThemedView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 15,
+  },
+  h1: {
+    fontSize: 100,
+    fontWeight: 'bold',
+    lineHeight: 'normal',
+    color: '#F26722',
+  },
+  h2: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    lineHeight: 36,
+  },
+  h3: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    lineHeight: 32,
+  },
+  img: {
+    width:'100%',
+    height:180,
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+  },
+  card: {
+    flex:1,
+    marginTop: 25,
+    marginBottom:25,
+  },
+  carddetails: {
+    paddingTop:15,
+    paddingLeft:25,
+    paddingRight:25,
+    paddingBottom:50,
+    backgroundColor: '#FDB917',
+    borderBottomLeftRadius:40,
+    borderBottomRightRadius:40,
+  },
+  p: {
+    fontSize: 16,
+    paddingTop: 10,
+  },
+  small: {
+    fontSize:12,
+  }
+});
