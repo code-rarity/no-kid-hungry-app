16896ed3c48e7821baa5901b18f5706bef69ebdc
Ajay Alkondon <aalkondon@AAlkondon-MacPro-16.local>
2024-10-09 16:48:39 -0500

    Reverted all custom login form edits

--- model/userAPI.ts
+++ model/userAPI.ts
@@ -1,3 +1,4 @@
+import { signOut } from 'aws-amplify/auth';
 import { deleteValueFor, saveKeyValue } from "@/helpers/misc";
 
 const LUMINATE_CONS_CLIENT_URL = "https://secure.nokidhungry.org/site/CRConsAPI";
@@ -6,13 +7,16 @@ const API_KEY = 'sosapikey';
 const VERSION = '1.0';
 const RESPONSE_FORMAT = 'json';
 
+// LUMINATE CRM IS DATABASE OF RECORD - PULL AND PUSH UPDATES //
 // ** External functions to use elsewhere in the app ** //
 
-// createOrGetAuthUser runs on app login to sync or create an user
-export const createOrGetAuthUser = async (payload) => {
+// Luminate functions for login, logout and more
+// fetchOrCreateLuminateUser gets or creates luminate user
+export const fetchOrCreateLuminateUser = async (payload) => {
   if(typeof payload !== 'undefined') {
-    const primaryEmail = payload.data.signInDetails.loginId;
     try {
+      // Email must be lowercase for subsequent API calls!!
+      const primaryEmail = (payload.data.signInDetails.loginId).toLowerCase();
       await fetch(`${LUMINATE_CONS_CLIENT_URL}?method=authenticateUser&api_key=${API_KEY}&v=${VERSION}&response_format=${RESPONSE_FORMAT}&primary_email=${primaryEmail}`, {
         method: 'POST',
         headers: {
@@ -25,13 +29,23 @@ export const createOrGetAuthUser = async (payload) => {
         if(typeof result.loginResponse !== 'undefined') {
           // Luminate CRM is database of record, so we must update dynamoDB with any changes
           // We do this on login, so that we dont have to keep querying Luminate's APIs
-          console.log("User successful luminate auth, so now get user data and sync w/ dynamoDB");
-          getSyncAuthUser(result.loginResponse.token);
+          console.log("Logged in user exists in Luminate - need to sync between Luminate and DynamoDB");
+          fetchAndSyncDBS(result.loginResponse.token);
         } else {
+          // Some type of error on Luminate authenticateUser call
           if(result.errorResponse.code == "16") {
-            console.log("User does not exist in Luminate, so now create user in Luminate and dynamoDB");
-            createAuthUser(primaryEmail);
+            try {
+              console.log("User does not exist in Luminate, so now create user in Luminate and dynamoDB");
+              createLuminateUser(primaryEmail);
+              // See if they are logged in automatically after create or not
+            } catch(error) {
+              // App logout user if they failed to get created in Luminate
+              signOut();
+              throw new Error("Could not create a luminate user, so logging out as Auth not synced");
+            }
           } else {
+            // Logout of the App login as the user either made a typo or doesn't exist in luminate
+            signOut();
             throw new Error("An error occurred with the API call! Cannot auth into Luminate.");
           }
         }
@@ -44,7 +58,7 @@ export const createOrGetAuthUser = async (payload) => {
   }
 }
 
-export const logOutUser = async () => {
+export const logOutLuminateUser = async () => {
   try {
     await fetch(`${LUMINATE_CONS_CLIENT_URL}?method=logout&api_key=${API_KEY}&v=${VERSION}&response_format=${RESPONSE_FORMAT}`, {
       method: 'POST',
@@ -66,9 +80,9 @@ export const logOutUser = async () => {
 
 // ** Internal functions to be used within other functions here ** //
 
-const getSyncAuthUser = (token) => {
+const fetchAndSyncDBS = (token) => {
   // Use Expo's Secure Store to safely store the luminate session token locally
-  saveKeyValue("luminateToken", token);
+  // saveKeyValue("luminateToken", token);
 
   try {
     fetch(`${LUMINATE_CONS_CLIENT_URL}?method=getUser&api_key=${API_KEY}&v=${VERSION}&response_format=${RESPONSE_FORMAT}&sso_auth_token=${token}`, {
@@ -88,7 +102,7 @@ const getSyncAuthUser = (token) => {
   }
 }
 
-const createAuthUser = (email) => {
+const createLuminateUser = (email) => {
   try {
     fetch(`${LUMINATE_CONS_CLIENT_URL}?method=create&api_key=${API_KEY}&v=${VERSION}&response_format=${RESPONSE_FORMAT}&primary_email=${email}`, {
       method: 'POST',
