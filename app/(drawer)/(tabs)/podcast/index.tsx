0f0203cf6b894f6d65c6f117f5c43a3ac60cbb14
Ajay Alkondon <aalkondon@AAlkondon-MacPro-16.local>
2024-10-08 21:09:18 -0500

    Latest model and crm integration edits

--- app/(drawer)/(tabs)/podcast/index.tsx
+++ app/(drawer)/(tabs)/podcast/index.tsx
@@ -4,14 +4,14 @@ import { ThemedView } from '@/components/ThemedView';
 import { useNavigationSearch } from '@/hooks/useNavigationSearch';
 import { TrackList } from '@/components/podcast/TrackList';
 import { screenPadding } from '@/constants/Layout';
-import { fetchMP3DataFromXML } from '@/model/podcastAPI';
+import { fetchPodcastTracks } from '@/model/podcastAPI';
 import { episodeTitleFilter } from '@/helpers/filters';
 
 export default function PodcastScreen() {
   const [episodes, setEpisodes] = useState([]);
 
   useEffect(() => {
-    fetchPodcastAudioTracks();
+    fetchTracks();
   }, []);
 
   const search = useNavigationSearch({
@@ -22,42 +22,10 @@ export default function PodcastScreen() {
     }
   });
 
-  const fetchPodcastAudioTracks = async () => {
-    const wordPressURL = "https://dev-share-our-strength1.pantheonsite.io";
-
-    try {
-      await fetchMP3DataFromXML()
-      .then((trackList) => {
-        const episodes = [];
-        fetch(`${wordPressURL}/wp-json/wp/v2/podcast?filter[orderby]=date&order=desc`)
-        .then(rep1 => rep1.json())
-        .then(res1 => {
-          (res1).map((episode, i) => {
-            fetch(`${wordPressURL}/wp-json/wp/v2/media/${episode.featured_media}`)
-            .then(rep2 => rep2.json())
-            .then(res2 => {
-              trackList.map((track, j) => {
-                if(track.title == episode.title.rendered) {
-                  setEpisodes(prevEpisodes => [
-                    ...prevEpisodes,
-                    {
-                      title: episode.title.rendered,
-                      date: (episode.date).split("T")[0],
-                      link: episode.link,
-                      image: (res2.source_url ? res2.source_url : track.image),
-                      content: episode.content.rendered,
-                      url: track.url,
-                    }
-                  ]);
-                }
-              });
-            })
-          });
-        });
-      });
-    } catch (error) {
-      console.log(error);
-    }
+  const fetchTracks = async () => {
+    await fetchPodcastTracks().then(response => {
+      console.log(response);
+    });
   }
 
   const filteredEpisodes = useMemo(() => {
